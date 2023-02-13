54a0f81c9703d85a47fc4014c32b152a314f5c38
olive <olive@10.0.0.179>
2023-02-12 19:26:51 -0700

    Week5Lab MyLogin Made by me Oliver Falak, Enjoy!

---
+++ src/java/model/AccountService.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ *
+ * @author olive
+ */
+public class AccountService {
+    
+    public User login(String username, String password){
+        
+    if((username.equals("abe") || username.equals("barb")) 
+                && password.equals("password")){
+            return new User(username, null);
+        }
+        else {
+            return null;
+        }
+    }
+}
